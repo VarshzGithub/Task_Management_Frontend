d9b4e921524a4e175c3cc56f9ad36d1c20056bcf
Sreevarshini <110294603+VarshzGithub@users.noreply.github.com>
2025-03-24 12:45:00 +0530

    Create AuthenticationResponse.java

---
+++ Full_stack_Training/Full-Stack-Project/securitybootspringnew/model/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.spring.bootsecuirty.secuirtybootspringnew.model;
+
+public class AuthenticationResponse {
+
+	public String token;
+	
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
