6605b0e478b09cd2ce35a036242f6216beb8918d
Sreevarshini <110294603+VarshzGithub@users.noreply.github.com>
2025-03-24 12:46:41 +0530

    Create RegisterRequest.java

---
+++ Full_stack_Training/Full-Stack-Project/securitybootspringnew/model/RegisterRequest.java
@@ -0,0 +1,54 @@
+package com.spring.bootsecuirty.secuirtybootspringnew.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+	public String username;
+
+	public String email;
+
+	public String password;
+	
+	private Role role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+}
