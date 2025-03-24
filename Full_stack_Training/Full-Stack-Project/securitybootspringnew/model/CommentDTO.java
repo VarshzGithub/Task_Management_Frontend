9c4f2c61c624e4bf4150f076b9e2f9e789b9b3e7
Sreevarshini <110294603+VarshzGithub@users.noreply.github.com>
2025-03-24 12:45:54 +0530

    Create CommentDTO.java

---
+++ Full_stack_Training/Full-Stack-Project/securitybootspringnew/model/CommentDTO.java
@@ -0,0 +1,25 @@
+package com.spring.bootsecuirty.secuirtybootspringnew.model;
+
+public class CommentDTO {
+    private String username;
+    private String text;
+    
+    
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+    // Getters and Setters
+    
+    
+}
+
