7138ff2c30234d3ae5073905b80c95bfae7e614c
Sreevarshini <110294603+VarshzGithub@users.noreply.github.com>
2025-03-24 12:54:39 +0530

    Create SecuirtybootspringnewApplication.java

---
+++ Full_stack_Training/Full-Stack-Project/securitybootspringnew/SecuirtybootspringnewApplication.java
@@ -0,0 +1,15 @@
+package com.spring.bootsecuirty.secuirtybootspringnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuirtybootspringnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuirtybootspringnewApplication.class, args);
+		System.out.println("Started....");
+		
+	}
+
+}
